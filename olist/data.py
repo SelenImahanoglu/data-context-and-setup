928eb7f15435baaa769077403981d0519d0f10b2
Macos <macos@Macos-MacBook-Air.local>
2026-02-18 16:03:50 +0300

    kick off olist challenge

--- olist/data.py
+++ olist/data.py
@@ -20,7 +20,19 @@ class Olist:
         Its keys should be 'sellers', 'orders', 'order_items' etc...
         Its values should be pandas.DataFrames loaded from csv files
         """
-        pass  # YOUR CODE HERE
+       def get_data(self):
+        # 1. Verilerin olduğu gizli global klasörü buluyoruz
+        csv_path = Path.home().joinpath(".workintech/olist/data/csv")
+
+        data = {}
+        # 2. Klasördeki tüm dosyaları tek tek dönüyoruz
+        for file in csv_path.iterdir():
+            if file.suffix == '.csv':
+                # İsim temizleme: 'olist_orders_dataset.csv' -> 'orders'
+                key = file.name.replace('olist_', '').replace('_dataset.csv', '').replace('.csv', '')
+                data[key] = pd.read_csv(file)
+
+        return data
 
     def ping(self):
         """
